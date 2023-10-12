cd29a1abf9efead4a5578771d3ef6b385b4b2776
davidkarp94 <davidkarp94@gmail.com>
2023-10-12 13:58:46 -0300

    header first design

--- ironvix/client/src/App.jsx
+++ ironvix/client/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Contacto from './pages/contacto/Contacto';
+import Home from './pages/home/Home';
+import Nosotros from './pages/nosotros/Nosotros';
+import Portfolio from './pages/portfolio/Portfolio';
+import Servicios from './pages/servicios/Servicios';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+
+      <Home />
+      <Portfolio />
+      <Servicios />
+      <Nosotros />
+      <Contacto />
+
+      <Footer />
     </>
   )
 }
